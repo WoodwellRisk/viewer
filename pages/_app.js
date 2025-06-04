cba7019b2139a74729e3430b98d0ddfc2b58664a
jakidxav <p6mmc@proton.me>
2025-06-04 09:23:07 -0700

    updating carbonplan pakcage versions; adding google analytics script to site

--- pages/_app.js
+++ pages/_app.js
@@ -4,6 +4,7 @@ import '@carbonplan/components/fonts.css'
 import '@carbonplan/maps/mapbox.css'
 // import './stylesheet.css'
 import theme from '@carbonplan/theme'
+import Script from 'next/script';
 
 const App = ({ Component, pageProps }) => {
   // console.log(theme)
@@ -16,9 +17,25 @@ const App = ({ Component, pageProps }) => {
   // }
 
   return (
-    <ThemeUIProvider theme={theme}>
-      <Component {...pageProps} />
-    </ThemeUIProvider>
+    <>
+      <Script
+        src="https://www.googletagmanager.com/gtag/js?id=G-4619MS74NJ"
+        strategy="afterInteractive"
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){window.dataLayer.push(arguments);}
+          gtag('js', new Date());
+
+          gtag('config', 'G-4619MS74NJ');
+        `}
+      </Script>
+
+      <ThemeUIProvider theme={theme}>
+        <Component {...pageProps} />
+      </ThemeUIProvider>
+    </>
   )
 }
 
