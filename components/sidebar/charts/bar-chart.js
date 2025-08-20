8a45f11458824c7301bccda5c1950d07aefdb7ab
jakidxav <p6mmc@proton.me>
2025-08-20 16:32:31 -0700

    users can now download bar chart data for all available time periods or warming levels

--- components/sidebar/charts/bar-chart.js
+++ components/sidebar/charts/bar-chart.js
@@ -1,3 +1,4 @@
+import { useMemo } from 'react'
 import { Box } from 'theme-ui'
 import { AxisLabel, Chart, Grid, Plot, Ticks, TickLabels } from '@carbonplan/charts'
 
@@ -35,46 +36,16 @@ const BarChart = () => {
         }
     }
 
-    let min = clim[0]
-    let max = clim[1]
-    const variableRange = [min, max]
-
     if (!regionData || !regionData[variable]) { // ex: if(!'drought' or Object["drought"]) {...}
         return
     }
 
     let lat = regionData.coordinates.lat;
     let lon = regionData.coordinates.lon;
-    let graphData = []
-    let graphLat = []
-    let graphLon = []
 
-    // for some layers, there are values that are above the ranges in climRanges, where I created a "truncated" colormap. 
-    // so for anything above or below those ranges, there is "no data" to show in the histogram.
-    // i solved this by setting values above and below the colormap values to the min / max of climRanges.
-    // https://stackoverflow.com/questions/22311544/get-indices-indexes-of-all-occurrences-of-an-element-in-an-array
-    let data;
-    if(variable.startsWith('cf')) {
-        data = regionData[variable][crop][band];
-    } else {
-        data = regionData[variable][band];
-    }
-
-    data.forEach(function (element, idx) {
-        if (element !== 9.969209968386869e36) {
-            if (element > max) {
-                graphData.push(max);
-            } else if (element < min) {
-                graphData.push(min);
-            } else {
-                graphData.push(element);
-            }
-
-            graphLat.push(lat[idx]);
-            graphLon.push(lon[idx]);
-        }
-    });
-    let dataCount = graphData.length
+    let min = clim[0]
+    let max = clim[1]
+    const variableRange = [min, max]
 
     // bin the data
     const nBins = 10;
@@ -95,27 +66,72 @@ const BarChart = () => {
     }
 
     const bin = d3.bin().domain(variableRange).thresholds(binEdges)
-    const bins = bin(graphData)
 
-    let percentages;
-    const initialValue = 0;
-    // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
-    let checkEmptyArrays = bins.reduce((total, arr) => total + arr.length, initialValue);
-    if (checkEmptyArrays == 0) { // then there is no data to display
-        percentages = Array(nBins + 1).fill(0)
-    } else {
-        percentages = bins.map(arr => {
-            return Number(((arr.length / dataCount) * 100).toFixed(0))
-        })
-    }
-
-    // https://stackoverflow.com/questions/22015684/zip-arrays-in-javascript
-    const zip = (x, y) => Array.from(Array(x.length), (_, i) => [x[i], y[i]]);
-    let plotData = zip(binEdges, percentages);
     const xMin = (variable == 'tavg') ? -35 : min - binWidth;
     const xMax = (variable == 'tavg') ? 35 : max + binWidth;
 
-    console.log(plotData)
+    // for some layers, there are values that are above the ranges in climRanges, where I created a "truncated" colormap. 
+    // so for anything above or below those ranges, there is "no data" to show in the histogram.
+    // i solved this by setting values above and below the colormap values to the min / max of climRanges.
+    // https://stackoverflow.com/questions/22311544/get-indices-indexes-of-all-occurrences-of-an-element-in-an-array
+    let data;
+    if(variable.startsWith('cf')) {
+        data = regionData[variable][crop];
+    } else {
+        data = regionData[variable];
+    }
+
+    let binnedData = useMemo(() => {
+        let bands = Object.keys(data).sort()
+        let binnedData = {}
+
+        bands.forEach(band => {
+            let graphData = []
+            data[band].forEach(function (element, idx) {
+                if (element !== 9.969209968386869e36) {
+                    if (element > max) {
+                        graphData.push(max);
+                    } else if (element < min) {
+                        graphData.push(min);
+                    } else {
+                        graphData.push(element);
+                    }
+                }
+            });
+            let dataCount = graphData.length
+
+            // bin the data
+            const bins = bin(graphData)
+
+            let percentages;
+            const initialValue = 0;
+            // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Reference/Global_Objects/Array/reduce
+            let checkEmptyArrays = bins.reduce((total, arr) => total + arr.length, initialValue);
+            if (checkEmptyArrays == 0) { // then there is no data to display
+                percentages = Array(nBins + 1).fill(0)
+            } else {
+                percentages = bins.map(arr => {
+                    return Number(((arr.length / dataCount) * 100).toFixed(0))
+                })
+            }
+
+            // https://stackoverflow.com/questions/22015684/zip-arrays-in-javascript
+            const zip = (x, y) => Array.from(Array(x.length), (_, i) => [x[i], y[i]]);
+            let binnedBand = zip(binEdges.map((edge) => edge), percentages);
+            binnedData[band] = binnedBand
+        })
+
+        // const sortedData = Object.keys(binnedData).sort().reduce((acc, key) => {
+        //     acc[key] = binnedData[key];
+        //     return acc;
+        // }, {});
+
+        // return sortedData;
+        return binnedData;
+    }, [data])
+
+    let plotData = binnedData[band];
+
     return (
         <>
             <Box sx={{ ...sx.chart }} className='chart-container'>
@@ -127,15 +143,12 @@ const BarChart = () => {
                     <AxisLabel left >Percent</AxisLabel>
                     <AxisLabel bottom>{chartLabel}</AxisLabel>
                     <Plot>
-                        <Bar 
-                            data={plotData} 
-                            strokeWidth={0.5}
-                        />
+                        <Bar data={plotData} strokeWidth={0.5} />
                     </Plot>
                 </Chart>
 
                 <Box sx={sx['data-download']}>
-                    Download data: <DownloadBarData data={plotData} fileType={'CSV'} /> / <DownloadBarData data={plotData} fileType={'JSON'} />
+                    Download data: <DownloadBarData data={binnedData} fileType={'CSV'} /> / <DownloadBarData data={binnedData} fileType={'JSON'} />
                 </Box>
 
             </Box>
